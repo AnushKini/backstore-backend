aa176156de7f0ffeba3ba23a10cd2eb033862edf
AnushKini <anushjkini@gmail.com>
2020-04-22 19:39:30 +0530

    changed sql dialect

--- target/generated-sources/annotations/com/bookstore/mapper/BookMapperImpl.java
+++ target/generated-sources/annotations/com/bookstore/mapper/BookMapperImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-04-22T19:28:42+0530",
+    date = "2020-04-22T19:38:45+0530",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 1.8.0_171 (Oracle Corporation)"
 )
 @Component
